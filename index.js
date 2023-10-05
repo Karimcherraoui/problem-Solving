7187fc40f706956207c1e0d623d19803605084dc
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-10-05 23:23:26 +0100

    premiere solution +  deuxieme solution

--- index.js
+++ index.js
@@ -21,29 +21,105 @@
 // Given two strings, write a method to determine if they are one edit away from each other.
 
 
-const string1 = "karim"
-const string2 = "kim"
-function checkLength(s1,s2){
-    return s1.length > s2.length ? s1.length : s2.length
-}
-let length = checkLength(string1,string2)
+// function checkLength(s1,s2){
+    //     return s1.length > s2.length ? s1 : s2
+    // }
+    // let str = checkLength(string1,string2)
+    
+    // console.log(str)
+    
+    
+    
+//     const string1 = "karim"
+//     const string2 = "kim"
+
+
+
+// function checkEdit(s1,s2){
+
+//     let char = []
+//     let index = []
+
+//     let arr1 = Array.from(s1);
+//     let arr2 = Array.from(s2);
+
+
+//     let l1 = arr1.length ;
+//     let l2 = arr2.length ;
+
+    
+
+//     if(Math.abs(l1 - l2)>2 ){
+//         console.log("vous avez besoin plus de 2 modification ")
+//     }else if ((l1 - l2) <= 2 && (l1 - l2) > 0 ) {
+//         for(i = 0 ; i < l1 ; i++ ){
+//             console.log(arr2[i])
+//             if(arr1[i] !== arr2[i]){
+//                 arr1.splice(i,1)
+//             }
+            
+//         }
+//         console.log(arr1,arr2)
+//     }
+
+//     return [char , index]
+
+// }
+
+
+// console.log(checkEdit(string1,string2));
+
+// checkEdit(string1,string2)
 
 
 
 
+const string1 = "oucod";
+const string2 = "youcode";
 
+function checkEdit(s1, s2) {
+  let arr1 = Array.from(s1);
+  let arr2 = Array.from(s2);
 
-function checkEdit(s1,s2){
+  let l1 = arr1.length;
+  let l2 = arr2.length;
+console.log(Math.abs(l1 - l2))
+  if (Math.abs(l1 - l2)>=3) {
+    console.log("Vous avez besoin plus de 2 modifications.");
+  }else{
 
-    if(string1.length !== string2.length){
-        return false
-    }else {
-        for (let i = 0 ; i < length ; i++ ){
-            if()
-        }
+  let i = 0;
+  while ((l1 - l2) <= 2 && (l1 - l2) > 0) {
+    if (arr1[i] !== arr2[i]) {
+      arr1.splice(i, 1);
+      l1--;
+    } else {
+      i++;
     }
+  }
+  while ((l1 - l2) < 0 ) {
+    if (arr2[i] !== arr1[i]) {
+        [
+            // part of the array before the specified index
+            ...arr1.slice(0, i),
+            // inserted item
+            arr2[i],
+            // part of the array after the specified index
+            ...arr1.slice(i)
+          ]
+      l2--;
+    } else {
+      i++;
+    }
+  }
+  
+  }
+  
+  return [arr1.join(''), arr2.join('')];
 
 
 }
 
-console.log(checkEdit(string1,string2));
+const [str1, str2] = checkEdit(string1, string2);
+console.log("Modified String 1:", str1);
+console.log("Modified String 2:", str2);
